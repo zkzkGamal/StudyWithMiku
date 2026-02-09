436b5874a9549816558775079897a9ff6f924070
ZkariaGamalAloha <zkaria.gamal@alohadot.net>
2026-02-09 12:19:13 +0200

    handle system istructions

--- core/tools.py
+++ core/tools.py
@@ -1,14 +1,16 @@
 from tools.network import network, openwifi, searchweb
 from tools.embedded import embedded_pdf
-from tools.browser import open_browser
+from tools.browser import open
 from tools.processes_tools import findProcess , killProcess
+from tools.system import run_command
 
 __all__ = [
     network.check_internet,
     openwifi.enable_wifi,
     searchweb.duckduckgo_search,
     embedded_pdf.embedded_pdf,
-    open_browser.open_browser,
+    open.open_browser,
     findProcess.find_process,
     killProcess.kill_process,
+    run_command.run_command,
 ]
